f3767057ad381900f6a0db0cf4ff23acabb8aab8
Jonathan Sidi <yonis@metrumrg.com>
2018-10-25 09:49:53 -0400

    fix appveyor.yml [skip travis]

--- tic.R
+++ tic.R
@@ -6,7 +6,7 @@ get_stage("after_script") %>%
 get_stage("deploy") %>%
   add_step(step_build_pkgdown())%>%
   add_step(step_setup_push_deploy(branch = "gh-pages",orphan = TRUE))%>%
-  add_step(step_do_push_deploy(path = 'docs'))
+  add_step(step_do_push_deploy(commit_paths = "docs/*"))
 
 get_stage("after_success") %>%
   add_code_step(setwd('c:/projects/covrpage'))%>%
